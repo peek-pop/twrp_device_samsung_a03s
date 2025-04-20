b6078fcdff964e950ae8bab86d8d396f31a025b9
peek-pop <68726024+peek-pop@users.noreply.github.com>
2025-04-20 11:26:32 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -24,8 +24,7 @@ PRODUCT_PACKAGES += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot@1.2-impl.recovery
 
 # Health Hal
 PRODUCT_PACKAGES += \
