9a55cbc3f0664e9b8dcfae5ce9898a847c2640e7
peek-pop <68726024+peek-pop@users.noreply.github.com>
2025-05-09 10:42:03 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -50,6 +50,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.bootimage.build.date.utc \
     ro.build.date.utc
-
-TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
