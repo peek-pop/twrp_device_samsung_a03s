5c17f3119b7116595f9a3c7ee2f96e368e652b76
peek-pop <68726024+peek-pop@users.noreply.github.com>
2026-02-26 23:46:23 +0700

    Update BoardConfig.mk for Samsung Galaxy A03s

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
 # Platform
-BOARD_VENDOR := MTK
+BOARD_VENDOR := samsung
 TARGET_SOC := Mediatek
 TARGET_BOARD_PLATFORM := MT6765
 TARGET_BOARD_PLATFORM_GPU := := PowerVR GE8320
@@ -113,6 +113,9 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.recovery.mt6768.rc
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# VINTF
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/recovery/root/vendor/etc/vintf/manifest.xml
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
@@ -189,6 +192,7 @@ TW_EXCLUDE_APEX := true
 TW_MAIN_VERSION := $(shell sed -n -e 's/^.*#define TW_MAIN_VERSION_STR //p' bootable/recovery/variables.h | cut -d'"' -f 2)
 CUSTOM_TWRP_VERSION := $(shell cat $(CUSTOM_TWRP_BUILD_NUMBER_FILE))
 TW_DEVICE_VERSION := Samsung Galaxy A03s
+
 
 
 
